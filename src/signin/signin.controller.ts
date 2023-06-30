48e53fd6276577f4e4c650f24755b7f9f340b3d5
RubsRafa <rubiarafaela2000@gmail.com>
2023-06-30 11:23:52 -0300

    feat: post /signin route

--- src/signin/signin.controller.ts
+++ src/signin/signin.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SigninService } from './signin.service';
+import { Signin } from './entity/Signin';
 
 @Controller('signin')
 export class SigninController {
   constructor(private readonly signinService: SigninService) {}
+
+  @Post()
+  signIn(@Body() body: Signin) {
+    return this.signinService.signIn(body);
+  }
 }
