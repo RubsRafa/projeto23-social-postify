24493df4d97bc1ed25b1dacb878a6d3bbec43f8f
RubsRafa <rubiarafaela2000@gmail.com>
2023-07-11 18:10:57 -0300

    feat: remove signin import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { SigninModule } from './signin/signin.module';
 import { PublicationsModule } from './publications/publications.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, SigninModule, PublicationsModule, PrismaModule],
+  imports: [UserModule, PublicationsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
