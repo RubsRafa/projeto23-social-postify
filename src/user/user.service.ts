c1e5769c0d7aaa1c5369bbca2ed7c3d91057feb2
RubsRafa <rubiarafaela2000@gmail.com>
2023-07-11 19:22:46 -0300

    feat: add bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersRepository } from './repository/user.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -9,7 +10,21 @@ export class UserService {
   async createUser(body: CreateUserDTO) {
     const user = await this.usersRepository.findUserByEmail(body.email);
     if (user) throw new HttpException('User already exists.', HttpStatus.CONFLICT);
-    return;
+
+    const hashedPassword = bcrypt.hashSync(body.password, 10);
+
+    const userCreated = await this.usersRepository.createUser({
+      ...body,
+      password: hashedPassword,
+    })
+
+    return {
+      id: userCreated.id,
+      name: userCreated.name,
+      email: userCreated.email,
+      avatar: userCreated.avatar,
+      createdAt: userCreated.createdAt,
+    }
   }
 
 }
