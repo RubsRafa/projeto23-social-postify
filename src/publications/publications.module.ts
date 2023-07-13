6824f159c969c61929cfa177656c91430623affd
RubsRafa <rubiarafaela2000@gmail.com>
2023-07-13 14:07:30 -0300

    fix: providers and exports on publications module

--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PublicationsService } from './publications.service';
 import { PublicationsController } from './publications.controller';
+import { PublicationRepository } from './repository/publication.repository';
+import { PrismaPublicationRepository } from './repository/implementations/prisma.publication.repository';
 
 @Module({
   controllers: [PublicationsController],
-  providers: [PublicationsService]
+  providers: [PublicationsService,
+  {
+    provide: PublicationRepository,
+    useClass: PrismaPublicationRepository,
+  },
+],
+exports: [
+  PublicationsService,
+  {
+    provide: PublicationRepository,
+    useClass: PrismaPublicationRepository,
+  }
+]
 })
 export class PublicationsModule {}
